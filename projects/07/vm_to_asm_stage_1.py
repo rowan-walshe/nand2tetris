2bc3d48ee7a1a481b0a0dd01b597b95a87fd3c35
Rowan W <rowan.walshe@altran.com>
2021-04-23 09:29:50 +0100

    Finished implementing push, and started optimising

--- projects/07/vm_to_asm_stage_1.py
+++ projects/07/vm_to_asm_stage_1.py
@@ -1,5 +1,7 @@
 import argparse
+from enum import Enum
 
+from os import path
 from copy import deepcopy
 from typing import List, Tuple
 
@@ -42,13 +44,28 @@ class VMLine:
                        "static",
                        "pointer",
                        "temp"}
+    
+    INC_SP = ["@SP", "M=M+1"]
+    DEC_SP = ["@SP", "M=M-1"]
+    PUT_D_ON_STACK = ["@SP", "A=M", "M=D"]
+    POP_STACK_ON_D = ["@SP", "A=M", "D=M"]
+    TEMP = 5
 
     def __init__(self,
                  original_line_number: int,
-                 original_input: str):
+                 original_input: str,
+                 file_name: str,
+                 add_comments: bool = False):
+        # Init attributes
         self.__line_number: int = original_line_number
         self.__original_input: str = original_input
-        self.__asm: Tuple = None
+        self.__asm: List = []
+        self.__file_name: str = file_name
+        self.__add_comments: str = add_comments
+        if add_comments:
+            self.__asm.append(f"// {original_input}")
+
+        # Convert origin input to asm
         self.__convert_vm_to_asm()
 
     def __convert_vm_to_asm(self):
@@ -67,9 +84,9 @@ class VMLine:
         return deepcopy(self.__original_input)
 
     def get_asm(self) -> Tuple[str]:
-        if self.__asm is None:
+        if not self.__asm or len(self.__asm) == int(self.__add_comments):
             raise VM_Error(f"Failed to convert VM command: '{self.__original_input}'")
-        return self.__asm
+        return tuple(self.__asm)
 
     def __validate_push_pop(self) -> List[str]:
         # A push/pop should have three distinct parts:
@@ -87,58 +104,238 @@ class VMLine:
 
     def _vm_push(self):
         parts = self.__validate_push_pop()
-        asm = [None, "D=M", "@SP", "M=D"]
         if parts[1] == "local":
-            pass
+            self.__asm.extend([f"@{parts[2]}", "D=A", "@LCL", "A=D+A", "D=M"])  # Put RAM[*LCL + i] into D
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "argument":
-            pass
+            self.__asm.extend([f"@{parts[2]}", "D=A", "@ARG", "A=D+A", "D=M"])  # Put RAM[*ARG + i] into D
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "this":
-            pass
+            self.__asm.extend([f"@{parts[2]}", "D=A", "@THIS", "A=D+A", "D=M"])  # Put RAM[*THIS + i] into D
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "that":
-            pass
+            self.__asm.extend([f"@{parts[2]}", "D=A", "@THAT", "A=D+A", "D=M"])  # Put RAM[*THAT + i] into D
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "constant":
-            asm[0] = f"@{parts[2]}"
-            asm[1] = "D=A"
+            self.__asm.extend([f"@{parts[2]}", "D=A"])
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "static":
-            pass
+            self.__asm.extend([f"@{self.__file_name}.{parts[2]}", "D=M"])
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "pointer":
-            pass
+            if parts[2] == '0':
+                self.__asm.extend(["@THIS", "D=M"])
+            elif parts[2] == '1':
+                self.__asm.extend(["@THAT", "D=M"])
+            else:
+                raise VM_Error(f"Push from pointer '{parts[2]}', but on values 0 or 1 are valid")
+            self.__asm.extend(self.PUT_D_ON_STACK)
         elif parts[1] == "temp":
-            pass
-        if None in asm:
+            offset = int(parts[2])
+            if 0 <= offset <= 7:
+                address = self.TEMP + int(parts)
+                self.__asm.extend([f"@{address}", "D=M"])
+                self.__asm.extend(self.PUT_D_ON_STACK)
+            else:
+                raise VM_Error(f"Trying to access temp memory segment out of range: 'RAM[*temp+{offset}]'")
+        else:
             raise VM_Error(f"Push from memory segment '{parts[1]}' not yet implemented")
-        self.__asm = tuple(asm)
+        self.__asm.extend(self.INC_SP)
 
     def _vm_pop(self):
         parts = self.__validate_push_pop()
         pass
 
     def _vm_add(self):
-        self.__asm = ("Add not yet implemented",)
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=D+M"])  # Add D to the value at the top of the stack, and push the result
+        self.__asm.extend(self.INC_SP)
 
     def _vm_sub(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=M-D"])  # Substract D from the value at top of stack, and push the result
+        self.__asm.extend(self.INC_SP)
 
     def _vm_neg(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=-M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.INC_SP)
 
     def _vm_eq(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M-D"])  # Substract D from the value at top of stack, and push the result
+        self.__asm.extend([
+            f"@TRUE.{self.__file_name}.{self.__line_number}",
+            "D;JEQ",
+            "@SP",
+            "A=M",
+            "M=0",
+            f"@END.{self.__file_name}.{self.__line_number}",
+            "0;JMP",
+            f"(TRUE.{self.__file_name}.{self.__line_number})",
+            "@SP",
+            "A=M",
+            "M=-1",
+            f"(END.{self.__file_name}.{self.__line_number})",
+        ])
+        self.__asm.extend(self.INC_SP)
 
     def _vm_gt(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M-D"])  # Substract D from the value at top of stack, and push the result
+        self.__asm.extend([
+            f"@TRUE.{self.__file_name}.{self.__line_number}",
+            "D;JGT",
+            "@SP",
+            "A=M",
+            "M=0",
+            f"@END.{self.__file_name}.{self.__line_number}",
+            "0;JMP",
+            f"(TRUE.{self.__file_name}.{self.__line_number})",
+            "@SP",
+            "A=M",
+            "M=-1",
+            f"(END.{self.__file_name}.{self.__line_number})",
+        ])
+        self.__asm.extend(self.INC_SP)
 
     def _vm_lt(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M-D"])  # Substract D from the value at top of stack, and push the result
+        self.__asm.extend([
+            f"@TRUE.{self.__file_name}.{self.__line_number}",
+            "D;JLT",
+            "@SP",
+            "A=M",
+            "M=0",
+            f"@END.{self.__file_name}.{self.__line_number}",
+            "0;JMP",
+            f"(TRUE.{self.__file_name}.{self.__line_number})",
+            "@SP",
+            "A=M",
+            "M=-1",
+            f"(END.{self.__file_name}.{self.__line_number})",
+        ])
+        self.__asm.extend(self.INC_SP)
 
     def _vm_and(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=D&M"])  # And D with the value at the top of the stack, and push the result
+        self.__asm.extend(self.INC_SP)
 
     def _vm_or(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "D=M"])  # Pop value at top of stack into D
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=D|M"])  # Or D with the value at the top of the stack, and push the result
+        self.__asm.extend(self.INC_SP)
 
     def _vm_not(self):
-        pass
+        self.__asm.extend(self.DEC_SP)
+        self.__asm.extend(["A=M", "M=!M"])  # Not the top of the stack, and push the result
+        self.__asm.extend(self.INC_SP)
+
+
+class ASM_Optimiser_State(Enum):
+    Null = 0
+    INC_DEC = 1
+    DEC_INC = 2
+    EITHER = 3
+
+
+class ASM_Optimiser:
+
+    SP_INC_DEC = ["@SP", "M=M+1", "@SP", "M=M-1"]
+    SP_DEC_INC = ["@SP", "M=M-1", "@SP", "M=M+1"]
+
+    def __init__(self, orig_asm: List[str]):
+        self.__orig_asm = orig_asm
+        self.__new_asm = []
+        self.__buffer = []
+        self.__comment_buff = []
+        self.__non_comment_buffer_length = 0
+        self.__last_check = len(self.__orig_asm) - 3
+        self.__state = ASM_Optimiser_State.Null
+
+        self.__optimise()
+
+    @property
+    def asm(self) -> List[str]:
+        return self.__new_asm
+
+    def __handle_Null(self, current_line):
+        if current_line == "@SP":
+            self.__state = ASM_Optimiser_State.EITHER
+            self.__buffer.append(current_line)
+            self.__non_comment_buffer_length += 1
+        else:
+            self.__new_asm.append(current_line)
+
+    def __handle_INC_or_DEC(self, current_line: str, lookup: List[str]):
+        if self.__non_comment_buffer_length == 2 and current_line == "@SP":
+            self.__buffer.append(current_line)
+            self.__non_comment_buffer_length += 1
+        elif self.__non_comment_buffer_length == 3 and current_line == lookup[3]:
+            self.__new_asm.extend(self.__comment_buff)
+            self.__new_asm.append("@SP")
+            self.__buffer = []
+            self.__comment_buff = []
+            self.__non_comment_buffer_length = 0
+            self.__state = ASM_Optimiser_State.Null
+        else:
+            self.__new_asm.extend(self.__buffer)
+            self.__new_asm.append(current_line)
+            self.__buffer = []
+            self.__comment_buff = []
+            self.__non_comment_buffer_length = 0
+            self.__state = ASM_Optimiser_State.Null
+
+    def __handle_EITHER(self, current_line: str):
+        if current_line == self.SP_INC_DEC[1]:
+            self.__state = ASM_Optimiser_State.INC_DEC
+            self.__buffer.append(current_line)
+            self.__non_comment_buffer_length += 1
+        elif current_line == self.SP_DEC_INC[1]:
+            self.__state = ASM_Optimiser_State.DEC_INC
+            self.__buffer.append(current_line)
+            self.__non_comment_buffer_length += 1
+        else:
+            self.__state = ASM_Optimiser_State.Null
+            self.__new_asm.extend(self.__buffer)
+            self.__new_asm.append(current_line)
+            self.__buffer = []
+            self.__comment_buff = []
+            self.__non_comment_buffer_length = 0
+
+    def __optimise(self):
+        for i in range(self.__last_check):
+            current_line = self.__orig_asm[i]
+            if self.__state != ASM_Optimiser_State.Null and current_line.startswith("//"):
+                self.__buffer.append(current_line)
+                self.__comment_buff.append(current_line)
+                continue
+            if self.__state == ASM_Optimiser_State.EITHER:
+                self.__handle_EITHER(current_line)
+            elif self.__state == ASM_Optimiser_State.INC_DEC:
+                self.__handle_INC_or_DEC(current_line, self.SP_INC_DEC)
+            elif self.__state == ASM_Optimiser_State.DEC_INC:
+                self.__handle_INC_or_DEC(current_line, self.SP_DEC_INC)
+            else:
+                self.__handle_Null(current_line)
+        self.__new_asm.extend(self.__buffer)
+        self.__new_asm.extend(self.__orig_asm[-3:])
 
 
 def remove_unwanted_whitespace(lines: List[Tuple[int, str]]) -> List[Tuple[int, str]]:
@@ -173,12 +370,14 @@ def remove_empty(lines: List[Tuple[int, str]]) -> List[Tuple[int, str]]:
     return new_lines
 
 
-def parse_vm(lines: List[str]) -> List[VMLine]:
+def parse_vm(lines: List[str],
+             file_name: str,
+             add_comments: bool) -> List[VMLine]:
     lines = list(enumerate(lines))
     lines = remove_unwanted_whitespace(lines)
     lines = remove_comments(lines)
     lines = remove_empty(lines)
-    lines = [VMLine(x, y) for x, y in lines]
+    lines = [VMLine(x, y, file_name, add_comments) for x, y in lines]
     return lines
 
 
@@ -186,15 +385,31 @@ def convert_vm_to_asm(vm_lines: List[VMLine]) -> List[str]:
     asm_lines = []
     for line in vm_lines:
         asm_lines.extend(line.get_asm())
+    
+    asm_lines.append("(INFINITE_LOOP)")
+    asm_lines.append("@INFINITE_LOOP")
+    asm_lines.append("0;JMP")
     return asm_lines
 
 
+def remove_redundant_sp_moves(asm_lines: List[str]) -> List[str]:
+    optimiser = ASM_Optimiser(asm_lines)
+    return optimiser.asm
+
+
 def parse_file(input_file_path: str,
-               output_file_path: str):
+               output_file_path: str,
+               add_comments: bool):
     with open(input_file_path, "r") as f:
         lines = f.readlines()
-    lines = parse_vm(lines)
+    # Get file name for static variable naming
+    file_name = input_file_path.rsplit('.', 1)[0]
+    file_name = path.split(file_name)[-1]
+    lines = parse_vm(lines, file_name, add_comments)
     lines = convert_vm_to_asm(lines)
+    lines = remove_redundant_sp_moves(lines)
+    # TODO add in rule to remove redundand SP++ followed by SP--
+    # TODO add in rule to remove redundand A instructions
     with open(output_file_path, "w+") as f:
         for line in lines:
             f.write(str(line))
@@ -203,7 +418,11 @@ def parse_file(input_file_path: str,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest="vm_file_path", type=str, help="Filepath of .vm program to be converted to .hack")
+    parser.add_argument(dest="vm_file_path", type=str, help="Filepath of .vm program to be converted to .asm")
+    parser.add_argument("--add_comments", action="store_true", help="Adds comments to generated assembly")
     args = parser.parse_args()
-    output_file_path = args.vm_file_path.rsplit('.', 1)[0] + ".hack"
-    parse_file(args.vm_file_path, output_file_path)
+    file_path_without_extension = args.vm_file_path.rsplit('.', 1)[0]
+    output_file_path = file_path_without_extension + ".asm"
+    parse_file(input_file_path=args.vm_file_path,
+               output_file_path=output_file_path,
+               add_comments=args.add_comments)
